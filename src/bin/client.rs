625d2587fb1f13077dba1b88c5ee1ec6440b64d2
Karl <karl@hashcloak.com>
2023-01-01 16:33:51 +0800

    first commit

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,5 +1,8 @@
 use clap::{Parser, Subcommand};
 use tokio::net::TcpStream;
+use bytes::{BytesMut, BufMut};
+//This for ?
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 //Define the supported command 
 #[derive(Subcommand,Debug)]
@@ -14,6 +17,8 @@ struct Cli {
     command : Command , 
 }
 
+//with such macro , we can make the main function async
+#[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
 
     let args = Cli::parse();
@@ -32,10 +37,28 @@ pub async fn main() -> Result<(), std::io::Error> {
             //write the value 
             stream.write_all(&value.as_bytes()).await?;
             
-            //S
+            let mut buf = BytesMut::with_capacity(1024);
+            let length = stream.read_buf(&mut buf).await?;
+            match std::str::from_utf8(&mut buf) {
+                Ok(uKey) => {
+                    if uKey == "key updated " {
+                        println!("key updated" , uKey);
+                    } else if uKey == "key set" {
+                        println!("key set" , uKey);
+                    }
+                }
+                Err(err) => {
+                    // failed to convert bytes into string slice
+                    println!("error: {}", err);
+                }
+
+                return Ok(());
+            }
+ 
         }
+        Ok(()) 
     }
     
 
-    //Read data from server 
+
 }
